fae5294382c891eb9f41214291872ce93392afc9
Krishal Shah <krishals.001@gmail.com>
2024-12-31 08:40:38 +0530

    added social links in the about section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,6 @@
+import SocialLink from '@/components/social-link';
+import { siteConfig } from '@/config';
+
 import NewsLetterForm from './_components/newsletter-form';
 import Introduction from './_sections/introduction';
 import RecentPosts from './_sections/recent-posts';
@@ -7,6 +10,12 @@ export default function Home() {
     <section className='py-24'>
       <div className='container max-w-3xl'>
         <Introduction />
+
+        <div className='flex mt-12 space-x-6 justify-center'>
+          {siteConfig.socialLinks.map((socialLinkProps) => (
+            <SocialLink key={socialLinkProps._id} {...socialLinkProps} />
+          ))}
+        </div>
 
         <RecentPosts />
 
